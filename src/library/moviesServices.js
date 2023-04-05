c8fe29b4a4b9d17e66e2c8a57a45093972b4a685
alirezamou <am808036@gmail.com>
2023-04-05 17:19:42 +0330

    add 'moviesServices.js' for working with movies in database

---
+++ src/library/moviesServices.js
@@ -0,0 +1,27 @@
+import { FirebaseStore } from "./firebase";
+import { collection, getDocs, getDoc, doc } from "firebase/firestore/lite";
+
+export const moviesCollectionRef = collection(FirebaseStore, "movies");
+
+export async function getMovies() {
+  const moviesSnapshot = await getDocs(moviesCollectionRef);
+
+  if (moviesSnapshot) {
+    const moviesDocs = moviesSnapshot.docs;
+    return moviesDocs.map((movieDoc) => movieDoc.data());
+  } else throw new Error("Something wrong went with fetching the movies");
+}
+
+export async function getMovie(id) {
+  if (!id) {
+    throw new Error("ID of the movie is not provided");
+  }
+
+  const movieDocRef = doc(FirebaseStore, "/movies/" + id);
+  const movieDoc = await getDoc(movieDocRef);
+  if (movieDoc) {
+    return movieDoc.data();
+  } else {
+    throw new Error(`Error occured with fetching the movie with id: ${id}`);
+  }
+}
