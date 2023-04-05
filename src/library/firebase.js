ee612943ea7cecc0d9fdc2b312ebb9368596f750
alirezamou <am808036@gmail.com>
2023-04-05 16:31:31 +0330

    fix - 'app' changed to 'FirestoreApp', import fix

--- src/library/firebase.js
+++ src/library/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore/lite";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -12,5 +12,5 @@ const firebaseConfig = {
 };
 
 export const FirebaseApp = initializeApp(firebaseConfig);
-export const FirebaseStore = getFirestore(app);
+export const FirebaseStore = getFirestore(FirebaseApp);
 export const FirebaseAuth = getAuth(FirebaseApp);
