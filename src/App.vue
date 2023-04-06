b139edfa629c11e0b5e3bc56f0bc7474894679a2
alirezamou <am808036@gmail.com>
2023-04-06 17:12:48 +0330

    add simple navbar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,30 @@
 </script>
 
 <template>
+  <nav class="navbar navbar-light navbar-expand bg-light">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">Vidly</router-link>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/movies" class="nav-link">Movies</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/customers" class="nav-link">Customers</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/rentals" class="nav-link">Rentals</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link">Register</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <router-view />
 </template>
 
