afe6339f885392a1f91f53942a6216f11020ede7
alirezamou <am808036@gmail.com>
2023-04-07 12:10:32 +0330

    route for '/login' page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import MoviesIndex from "@/components/MoviesIndex.vue";
+import Login from "@/components/Login.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: "movies",
       path: "/",
       component: MoviesIndex,
+    },
+    {
+      name: "login",
+      path: "/login",
+      component: Login,
     },
   ],
 });
