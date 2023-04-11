a5d3374aee52e92be099d7b154f274cc3b15bccf
alirezamou <am808036@gmail.com>
2023-04-11 11:36:05 +0330

    add authService module for handling authentication

---
+++ src/library/authServices.js
@@ -0,0 +1,65 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { FirebaseAuth } from "@/library/firebase";
+
+export async function login(email, password) {
+  if (email === "" || password === "") {
+    throw new Error("Email or Password is empty");
+  } else {
+    try {
+      const response = await signInWithEmailAndPassword(
+        FirebaseAuth,
+        email,
+        password
+      );
+      const user = {
+        uid: response.user.uid,
+        email: response.user.email,
+      };
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export async function register(email, password) {
+  if (email === "" || password === "") {
+    throw new Error("Email or Password is empty");
+  } else {
+    try {
+      const response = await createUserWithEmailAndPassword(
+        FirebaseAuth,
+        email,
+        password
+      );
+      const user = {
+        uid: response.user.uid,
+        email: response.user.email,
+      };
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export async function signout() {
+  try {
+    await signOut(FirebaseAuth);
+  } catch (error) {
+    throw error;
+  }
+}
+
+export function isAuthenticated() {
+  if (FirebaseAuth.currentUser !== null) {
+    return true;
+  } else {
+    return false;
+  }
+}
